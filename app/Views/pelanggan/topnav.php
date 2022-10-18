ab5c7af66e31b2cc2ab320db9eb27421c889b27b
mzulfi3100 <mzulfikar3100@gmail.com>
2022-10-18 12:21:40 +0700

    add front end beli_produk

--- app/Views/pelanggan/topnav.php
+++ app/Views/pelanggan/topnav.php
@@ -12,9 +12,9 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome Icons -->
-  <link rel="stylesheet" href="/assets/AdminLTE/plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="/Assets/AdminLTE/plugins/fontawesome-free/css/all.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="/assets/AdminLTE/dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="/Assets/AdminLTE/dist/css/adminlte.min.css">
 </head>
 <body class="hold-transition layout-top-nav">
 <div class="wrapper">
@@ -61,22 +61,12 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
+    
     <!-- /.content-header -->
 
     <!-- Main content -->
-    <div class="content">
-        <div class="container">
+    <div class="content" style="background-color: #ffffff">
+        <div class="container; p-3" >
             <?= $this->renderSection('content') ?>
         </div>
     </div>
@@ -98,12 +88,12 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <!-- REQUIRED SCRIPTS -->
 
 <!-- jQuery -->
-<script src="/assets/AdminLTE/plugins/jquery/jquery.min.js"></script>
+<script src="/Assets/AdminLTE/plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
-<script src="/assets/AdminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="/Assets/AdminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
-<script src="/assets/AdminLTE/dist/js/adminlte.min.js"></script>
+<script src="/Assets/AdminLTE/dist/js/adminlte.min.js"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="/assets/AdminLTE/dist/js/demo.js"></script>
+<script src="/Assets/AdminLTE/dist/js/demo.js"></script>
 </body>
 </html>
