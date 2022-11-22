0610e71ebef1ff8791273180f95f35a9b9480611
Riski Sujianto Ramadhan <83487523+RiskiSujiantoRamadhan@users.noreply.github.com>
2022-11-22 18:55:20 +0700

    Update landing_page.php

--- app/Views/home/landing_page.php
+++ app/Views/home/landing_page.php
@@ -33,12 +33,8 @@
                 <div class="col-lg-9">
                     <div class="hero__search">
                         <div class="hero__search__form">
-                            <form action="#">
-                                <!-- <div class="hero__search__categories">
-                                    All Categories
-                                    <span></span>
-                                </div> -->
-                                <input type="text" placeholder="What do yo u need?">
+                            <form action="/dashboard_cari" method="get">
+                                <input type="text" name="cari" placeholder="What do yo u need?">
                                 <button type="submit" class="site-btn">SEARCH</button>
                             </form>
                         </div>
