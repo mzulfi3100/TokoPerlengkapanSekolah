2f1e4561c542f06854713fd2ed6f48c04c292e89
Riski Sujianto Ramadhan <83487523+RiskiSujiantoRamadhan@users.noreply.github.com>
2022-11-22 18:32:58 +0700

    Add files via upload

---
+++ app/Models/ProfileModel.php
@@ -0,0 +1,19 @@
+<?php
+ 
+namespace App\Models;
+ 
+use CodeIgniter\Model;
+ 
+class ProfileModel extends Model
+{
+    protected $DBGroup = 'default';
+    protected $table = "users";
+    protected $primaryKey = "id";
+    protected $useAutoIncrement = true;
+    protected $allowedFields = ['email', 'nama_lengkap', 'no_telepon', 'jenis_kelamin', 'alamat'];
+    protected $useTimestamps = false;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+}
